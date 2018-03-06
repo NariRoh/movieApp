dc49f9272b8072f7ef026bf41cfe34c070f4cc00
Nari Roh <nari.rohh@gmail.com>
2018-03-05 21:10:57 -0600

    Add prop-types

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-]
+const movies = [
+  {
+    title: "Matrix",
+    poster:   "https://upload.wikimedia.org/wikipedia/en/0/06/Ultimate_Matrix_Collection_poster.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster:   "https://resizing.flixster.com/LtoVIF3TxiP3c9wtzPscteMXqG4=/206x305/v1.bTsxMTE2ODAyOTtqOzE3Njc5OzEyMDA7ODAwOzEyMDA"
+  },
+  {
+    title: "Oldboy",
+    poster:   "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster:   "http://img.moviepostershop.com/star-wars-movie-poster-1977-1010460445.jpg"
+  },
 
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/0/06/Ultimate_Matrix_Collection_poster.jpg",
-  "https://resizing.flixster.com/LtoVIF3TxiP3c9wtzPscteMXqG4=/206x305/v1.bTsxMTE2ODAyOTtqOzE3Njc5OzEyMDA7ODAwOzEyMDA",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "http://img.moviepostershop.com/star-wars-movie-poster-1977-1010460445.jpg"
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {movies.map( (movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
