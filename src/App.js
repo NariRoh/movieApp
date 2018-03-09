e7cd8f44aa9064daa6e5d7982cc617c6a39f806c
Nari Roh <nari.rohh@gmail.com>
2018-03-09 10:38:52 -0600

    Add CSS

--- src/App.js
+++ src/App.js
@@ -4,108 +4,49 @@ import Movie from './Movie';
 
 
 class App extends Component {
-  // Render: componentWillMount() -> render() -> componentDidMount()
 
-  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() ->
-  // when component gets new props / when component(old and new one) is different, it's true /
+  state = {}
 
-  // componentWillMount() {
-  //   console.log('will mount');
-  //   // when componentWillMount, we'll request API
-  // }
-  //
-  // componentDidMount() {
-  //   console.log('did mount');
-  //   // means coponent is successfully rendered
-  // }
-
-  // state is an object which is inside every React components. whenever the state inside a component changes, the component renders agian
-
-  state = {
-    // greeting: 'Hello!',
-  }
-
-  // After the component renders
-  // add one more movie
   componentDidMount() {
-    // setTimeout(() => {
-    //   // this.state.greeting = 'something' (X) Do not write state directly!! Use setState()
-    //   this.setState({
-    //     greeting: 'Hello agian!'
-    //   })
-    // }, 5000)
-        // movies: [
-        //   ...this.state.movies,
-        //   // 👆 set the state of movies array like this: leave the array as it is (leave the list of the movies) and add one more movie.
-        //   // without this line, it will replace the list
-        //   {
-        //     title: "Trainspotting",
-        //     poster: "https://resizing.flixster.com/NCi30AewMxJnsjlAHJLaENLECLU=/206x305/v1.bTsxMTE3NjEwNztqOzE3Njc5OzEyMDA7ODAwOzEyMDA"
-        //   }
-        //   // ...this.state.movies
-        //   // 👆 when this line is here, it adds the new one on the top
-        // ]
-    // Fetch() returns a promise, A promise() gives you the abaility to do .then and .catch(to handle sinarios)
-
-    // fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-    // .then(response => response.json())
-    // .then(json => console.log(json))
-    // .catch(err => console.log(err))
-
-    // To avoid CALLBACK HELL we use async & await
-    // spread your functions  which are inside componentDidMount
     this._getMovies();
   }
 
-  // 👇 _underscore for function name: to make React fn and your own fn different since React has lots of functions
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      // console.log(movie);
       return <Movie
         title={movie.title_english}
-        poster={movie.medium_cover_image}
+        poster={movie.large_cover_image}
         genres={movie.genres}
         synopsis={movie.synopsis}
-        key={movie.id}
-      />
-    })
-    /*
-      👆 same as,
-      const movies = [<Movie props />, <Movie props />]
-    */
+        key={movie.id} />
+    });
     return movies;
   }
 
   _getMovies = async () => {
-    const movies = await this._callApi()
-    //
+    const movies = await this._callApi();
     this.setState({
-      // movies: movies
       movies
-    })
+    });
   }
 
   _callApi = () => {
+    // return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
     return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
     .then(response => response.json())
+    // .then(json => console.log(json))
+
     .then(json => json.data.movies)
-    // .then(json => json.data.movie)
-    // arrow function if there is no {}, no need to put 'return '
     .catch(err => console.log(err))
   }
 
   render() {
-    // console.log('did render');
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {/* {this.state.greeting} */}
-        {/* {this.state.movies.map( (movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })} */}
-        {/* whenever the movies are not in the state, we'll show 'Loading' or the list of movies */}
-        {/* When you use API, data won't be saved in the state so we used this ternary statement to ask wether we have the data saved or not */}
-        {this.state.movies ? this._renderMovies() : 'Loading'}
-        {/* 👆 for that you need a state object whatever it's empty state = {} */}
+      <div className={movies ? "App" : "App-loading"}>
+
+       {/* <div className={movies ? "App" : "App-loading"}> */}
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,52 +1,45 @@
-// import React, { Component } from 'react';
-// 👇 with functional component
 import React from 'react';
-
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
-
-// class Movie extends Component {
-//
-//   // static propTypes = {
-//   //   // title: PropTypes.string,
-//   //   title: PropTypes.string.isRequired,
-//   //   poster: PropTypes.string.isRequired
-//   // }
-//
-//   render() {
-//     console.log(this.props);
-//     return(
-//       <div>
-//         <MoviePoster poster={this.props.poster} />
-//         <h1>{this.props.title}</h1>
-//       </div>
-//     );
-//   }
-// }
 
 function Movie({poster, title, genres, synopsis}) {
   return (
     <div className="Movie">
-      <div className="Movie__Colums">
-        <MoviePoster poster={poster} alt={title}/>
+      <div className="Movie__Column">
+        <MoviePoster poster={poster} alt={title} />
       </div>
-      <div className="Movie__Colums">
+      <div className="Movie__Column">
         <h1>{title}</h1>
-        <div className="Movie__Colums">
-          {genres.map((genre, index) => <MovieGenre genre={genre}  key={index} />)}
-        </div>
-        <p className="Movie__Synopsis">
-          {synopsis}
-        </p>
+        <div className="Movie__Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
       </div>
-
+        {/* <div className="Movie__Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre}  key={index} />)}
+        </div> */}
+        <div className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            baseOn='letters'
+          />
+        </div>
+      </div>
     </div>
+  );
+}
+
+function MoviePoster({poster, alt}) {
+  return (
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
   );
 }
 
 function MovieGenre({genre}) {
   return (
-    <span className="Movie_Genre">{genre}</span>
+    <span className="Movie__Genre">{genre}</span>
   );
 }
 
@@ -57,42 +50,13 @@ Movie.propTypes = {
   synopsis: PropTypes.string.isRequired,
 }
 
-MovieGenre.propTypes = {
-  genre: PropTypes.string.isRequired
-}
-
-/*
-  * Samrt and Dumb Components
-  when a component has a state, it's a smart one. When it only has props instead it's a Dumb one.
-  For the dumb component(no state only props), instead of using a class component we can chain them into a function component(stateless functional component)
-*/
-// class MoviePoster extends Component {
-//
-//   static propTypes = {
-//     poster: PropTypes.string.isRequired
-//   }
-//
-//   render() {
-//     return(
-//       <img src={this.props.poster} alt={this.props.title} />
-//       // <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNGNiNWQ5M2MtNGI0OC00MDA2LWI5NzEtMmZiYjVjMDEyOWYzXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX182_CR0,0,182,268_AL_.jpg" />
-//     );
-//   }
-// }
-
-/*
-  Some components exist only to return something which don't need functions like componentWillMount, componentDidMount, update the state or render fn etc. All they need is just one props(like poster: needs one props and one HTML tag). All they need to do is to return somethign like a html
-*/
-
-function MoviePoster({poster, alt}) {
-  return (
-    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
-  );
-}
-
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
 }
 
 export default Movie;
