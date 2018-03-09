0c8d9e40427255581c578e4d8364f086bd314d93
Nari Roh <nari.rohh@gmail.com>
2018-03-08 19:41:27 -0600

    Updating Component

--- src/Movie.js
+++ src/Movie.js
@@ -24,18 +24,41 @@ import './Movie.css';
 //   }
 // }
 
-function Movie({poster, title}) {
+function Movie({poster, title, genres, synopsis}) {
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Colums">
+        <MoviePoster poster={poster} alt={title}/>
+      </div>
+      <div className="Movie__Colums">
+        <h1>{title}</h1>
+        <div className="Movie__Colums">
+          {genres.map((genre, index) => <MovieGenre genre={genre}  key={index} />)}
+        </div>
+        <p className="Movie__Synopsis">
+          {synopsis}
+        </p>
+      </div>
+
     </div>
+  );
+}
+
+function MovieGenre({genre}) {
+  return (
+    <span className="Movie_Genre">{genre}</span>
   );
 }
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
+}
+
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
 }
 
 /*
@@ -61,14 +84,15 @@ Movie.propTypes = {
   Some components exist only to return something which don't need functions like componentWillMount, componentDidMount, update the state or render fn etc. All they need is just one props(like poster: needs one props and one HTML tag). All they need to do is to return somethign like a html
 */
 
-function MoviePoster({poster, title}) {
+function MoviePoster({poster, alt}) {
   return (
-    <img src={poster} alt={title} />
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
   );
 }
 
 MoviePoster.propTypes = {
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
 }
 
 export default Movie;
